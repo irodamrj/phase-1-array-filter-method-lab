c44b6dc622a57c0816fbd01443ca2f874c14d1bd
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-25 02:13:55 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(item => item.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array, letters){
+    return array.filter(item => item.toLowerCase().startsWith(letters.toLowerCase()));
+}
+function matchName(array, name){
+    return array.filter(item => item.name === name);
+}
